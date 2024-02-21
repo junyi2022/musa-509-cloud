5541d74e4618274a9fa4869427e68203bbad4b21
junyi2022 <junyi2022@berkeley.edu>
2024-02-21 15:04:53 -0500

    add notes

--- week05/snippets/opa_properties.mjs
+++ week05/snippets/opa_properties.mjs
@@ -11,5 +11,5 @@ const f = fs.createWriteStream('../exercises/opa_properties_public.jsonl');
 for (const feature of data.features) {
   const row = feature.properties;
   row.geog = JSON.stringify(feature.geometry);
-  f.write(JSON.stringify(row) + '\n');
+  f.write(JSON.stringify(row) + '\n'); // '\n' is to make a new line for each write
 }
